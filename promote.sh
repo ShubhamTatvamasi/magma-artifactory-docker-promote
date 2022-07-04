836d329aac21ff1195613605921eb017c5d12b80
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-07-04 20:00:55 +0530

    Update promote.sh

--- promote.sh
+++ promote.sh
@@ -7,7 +7,7 @@ NEW_MAGMA_TAG=1.7.1
 MAGMA_ARTIFACTORY=artifactory.magmacore.org
 
 declare -A repositories=(
-  [orc8r]="controller magmalte nginx"
+  [orc8r]="controller magmalte nginx db-service dp-postgres"
   [feg]="gateway_go gateway_python"
   [agw]="agw_gateway_c agw_gateway_python ghz_gateway_c ghz_gateway_python"
   [cwf]="cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond operator"
