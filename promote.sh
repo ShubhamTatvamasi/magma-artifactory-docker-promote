abb197d54171dc9406b52d11082df45019f0a95b
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-09-14 22:45:23 +0530

    Update promote.sh

--- promote.sh
+++ promote.sh
@@ -7,11 +7,10 @@ NEW_MAGMA_TAG=1.8.0
 MAGMA_ARTIFACTORY=artifactory.magmacore.org
 
 declare -A repositories=(
-#   [orc8r]="controller magmalte nginx active-mode-controller configuration-controller radio-controller db-service"
-#   [feg]="gateway_go gateway_python"
-#   [agw]="agw_gateway_c agw_gateway_python ghz_gateway_c ghz_gateway_python agw_gateway_c_arm agw_gateway_python_arm"
-  [agw]="agw_gateway_c_arm agw_gateway_python_arm"
-#   [cwf]="cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond operator"
+  [orc8r]="controller magmalte nginx active-mode-controller configuration-controller radio-controller db-service"
+  [feg]="gateway_go gateway_python"
+  [agw]="agw_gateway_c agw_gateway_python ghz_gateway_c ghz_gateway_python agw_gateway_c_arm agw_gateway_python_arm"
+  [cwf]="cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond operator"
 )
 
 for repo in ${!repositories[@]}; do
